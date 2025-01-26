c2608b5f9c3b7ecb6cdf322ea19aa621f60d5198
MathiasVeraM <mathias.vera@udla.edu.ec>
2025-01-26 16:16:00 -0500

    Se creo el modelo Usuario (User) y la App DB context, ademas se descargo los paquetes nugget necesarios

---
+++ PAELoginAPI/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PAELoginAPI.Models;
+
+namespace PAELoginAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base (options)
+        {            
+        }
+        public DbSet<User> users { get; set; }
+    }
+}
